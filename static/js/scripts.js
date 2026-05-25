59b019906e3f48834d5c1e11da42987112cd37f8
zpneal <zpneal@msu.edu>
2026-05-25 10:06:55 -0400

    Add files via upload

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'overview', `faq', 'team']
+const section_names = ['home', 'overview', 'team']
 
 
 window.addEventListener('DOMContentLoaded', event => {
